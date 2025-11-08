4f067c291ba71f6e253d7c007ec289f46719a252
Abinban <bintangfernanda007@gmail.com>
2025-11-08 22:56:54 +0700

    img

--- src/assets/components/card/Card.jsx
+++ src/assets/components/card/Card.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 export default function Card({image, header, price}) {
   return (
-    <div className='flex w-40 h-[257px] pb-13 flex-col items-start'>
+    <div className='flex w-40 h-[257px] p-2.5 flex-col justify-evenly rounded-[20px]'>
       <img src={image} alt="" />
       <p className='font-inter text-[12px] font-medium leading-3 text-left'>{header}</p>
       <p className='font-inter text-[14px] font-semibold leading-5 text-left'>{price}</p>
-      <button className='rounded-2xl border-{1px} border-{#FF5722} border-solid'>+ Keranjang</button>
+      <a href="#" className='w-full'>
+        <div className='border-1 border-solid border-[#FF5722] w-full rounded-2xl text-center'>+ Keranjang</div>
+      </a>
     </div>
   )
 }
