24b58f1385e2972fa9da4d163582270230549921
Abinban <bintangfernanda007@gmail.com>
2025-11-08 23:51:53 +0700

    card

---
+++ src/assets/components/card/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Card({image, header, price}) {
+  return (
+    <div className='flex w-40 h-[257px] pb-2.5 flex-col justify-evenly rounded-[20px]'>
+      <img src={image} alt="" className='justify-self-start' />
+      <p className='font-inter text-[12px] font-medium leading-3 text-left'>{header}</p>
+      <p className='font-inter text-[14px] font-semibold leading-5 text-left'>{price}</p>
+      <a href="#" className='w-full'>
+        <div className='border-1 border-solid border-[#FF5722] w-full rounded-2xl text-center'>+ Keranjang</div>
+      </a>
+    </div>
+  )
+}
+
